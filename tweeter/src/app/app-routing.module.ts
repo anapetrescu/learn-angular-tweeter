2336876cddc55e34aa1f30c0dc2b6780376480cc
Petrescu Ana-Maria <32705011+anapetrescu@users.noreply.github.com>
2022-04-08 22:39:33 +0300

    Initial commit

---
+++ tweeter/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { RegisterComponent } from './auth/register/register.component';
+
+
+const routes: Routes = [
+  {path: '', component: AuthComponent},
+  {path: 'register', component: RegisterComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
